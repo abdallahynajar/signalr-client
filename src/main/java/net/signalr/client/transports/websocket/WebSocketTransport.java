06669973f3cbbc4aa82d7bf3831f9a8077763bbb
Martin Tamme <mtamme@gmx.at>
2013-12-08 19:33:21 +0100

    Initial commit.

--- src/main/java/net/signalr/client/transports/websocket/WebSocketTransport.java
+++ src/main/java/net/signalr/client/transports/websocket/WebSocketTransport.java
@@ -31,7 +31,6 @@ import com.ning.http.client.websocket.WebSocketUpgradeHandler;
 import net.signalr.client.Connection;
 import net.signalr.client.concurrent.Function;
 import net.signalr.client.concurrent.Futures;
-import net.signalr.client.concurrent.ImmediateFuture;
 import net.signalr.client.transports.AbstractTransport;
 import net.signalr.client.util.URIBuilder;
 
