93531c6f73fe60444f89a35684ea0b53f0275406
Martin Tamme <mtamme@gmx.at>
2013-12-09 08:42:59 +0100

    Initial commit.

---
+++ src/main/java/net/signalr/client/transports/Session.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © Martin Tamme
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.signalr.client.transports;
+
+import java.io.Closeable;
+import java.util.concurrent.Future;
+
+public interface Session extends Closeable {
+
+	String getConnectionData();
+
+	String getConnectionToken();
+
+	Future<Void> send(String message);
+}
